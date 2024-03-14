a24da230b73639dc9e4c58e98bd20252d3eeda16
VictorTadashi <victortadashisaitobarra@gmail.com>
2024-03-14 08:12:18 -0300

    Aula Alex

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,3 +1,5 @@
+import { AlertController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,7 +8,32 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  radioSelecionada:string=''
+  constructor(
+    public toastController: ToastController,
+    public alertController: AlertController
+  ) {}
 
-  constructor() {}
+  verificarRadio(){
+
+    this.exibirToast();
+    this.exibirAlerta();
+  }
+
+  async exibirToast(){
+    const toast = await this.toastController.create({
+      message: this.radioSelecionada,
+      duration: 2000,
+    });
+    toast.present();
+  }
+  async exibirAlerta(){
+    const alert = await this.alertController.create({
+      header: 'Esta é uma mensagem de exemplos.',
+      message: this.radioSelecionada,
+      buttons: ['OK']
+    });
+    alert.present()
+  }
 
 }
